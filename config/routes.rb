98e6887d9175f909ffa1a9fbc635ba20335d0b95
rickyuan <rushyzt@gmail.com>
2011-11-14 00:21:27 +0800

    Added user controller and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
 
   get "pages/about"
+  
+  root :to => "pages#home"
+  
+  match '/home',    :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/signup',  :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
